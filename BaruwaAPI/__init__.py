153f00813113981a19f788bff4aeb8b4b06d2f69
Andrew Colin Kissa <andrew@topdog.za.net>
2019-01-16 10:10:18 +0200

    PKG: Update copyright dates

--- BaruwaAPI/__init__.py
+++ BaruwaAPI/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4
 # BaruwaAPI Python bindings for Baruwa REST API
-# Copyright (C) 2015-2016 Andrew Colin Kissa <andrew@topdog.za.net>
+# Copyright (C) 2015-2019 Andrew Colin Kissa <andrew@topdog.za.net>
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
@@ -11,10 +11,10 @@ BaruwaAPI
 """
 from BaruwaAPI.resource import BaruwaAPIClient
 
-# pylint: disable=invalid-name,bad-builtin
-version_info = (0, 0, 3)
+# pylint: disable=invalid-name
+version_info = (0, 0, 4)
 __author__ = "Andrew Colin Kissa"
-__copyright__ = u"© 2015-2016 Andrew Colin Kissa"
+__copyright__ = u"© 2015-2019 Andrew Colin Kissa"
 __email__ = "andrew@topdog.za.net"
 __version__ = ".".join(map(str, version_info))
 
--- BaruwaAPI/resource.py
+++ BaruwaAPI/resource.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4
 # BaruwaAPI Python bindings for Baruwa REST API
-# Copyright (C) 2015-2016 Andrew Colin Kissa <andrew@topdog.za.net>
+# Copyright (C) 2015-2019 Andrew Colin Kissa <andrew@topdog.za.net>
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
